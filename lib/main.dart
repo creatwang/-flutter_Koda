f5255905a4f4c2f14ce1aa0c63bc6b0976002380
yanan <1690333108@qq.com>
2026-04-27 16:52:05 +0800

    fix: 价格保留2位小数

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:george_pick_mate/app/bootstrap/app_shell.dart';
 
 Future<void> main() async {
-  ///  仅横屏下使用后面需要兼容的化，去掉下面的代码
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
   ]);
   runApp(const ProviderScope(child: AppShell()));
 }
